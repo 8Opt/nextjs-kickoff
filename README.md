3d451274e475d6199fd4ee95603c97d0a9c3355f
MinLee0210 <bastreet124@gmail.com>
2025-03-02 23:28:35 +0700

    learning nextjs: chapter 6

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-## Next.js App Router Course - Starter
+# Next.js App Router Course - Starter
+
+
+
+
+## Setup Environment
+
+### Install neccessaries libraries
+
+
+### Setup Database
+
+```
+docker compose -f docker-compose.db.yml up -d
+```
+
+
+## Acknowledgement
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -2,7 +2,10 @@ import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+console.log("Connect to PostgreSQL: ", process.env.POSTGRES_URL!)
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: false });
 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
